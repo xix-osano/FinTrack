ed4b7fd7a8a5abf7107ec9640b3843953fac7303
xix-osano <osanoenosh@gmail.com>
2025-07-26 21:41:41 +0300

    updated db

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI_PRODUCTION, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true, // Use createIndex for better performance
-        });
+        const conn = await mongoose.connect(process.env.MONGO_URI_PRODUCTION);
         console.log(`MongoDB Successfully Connected: ${conn.connection.host}`);
     } catch (err) {
         console.error(`Error: ${err.message}`);
